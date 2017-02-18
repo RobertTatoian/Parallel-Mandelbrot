4a7abac42c3cffdb9a54b6625f3e5d39f7fa74e9
Robert Tatoian <rtatoian@cs.uri.edu>
2017-02-18 13:14:49 -0500

    Sync to work on other computer.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
 		int imageWidth = 2000;
 		int imageHeight = 2000;
+
 		ImageManager imageManager = new ImageManager(imageWidth, imageHeight);
+
 		Mandelbrot mandelbrot = new Mandelbrot(imageManager);
 		mandelbrot.iterateMandelbrot(1, 1, 0, 0);
 		UserInterface gui = new UserInterface(imageManager);
