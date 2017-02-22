65e89e8956488e2bedae3175ac56dea6f7253794
Robert Tatoian <rtatoian@outlook.com>
2017-02-22 13:22:33 -0500

    Rewriting to use atomic variables

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,33 +1,25 @@
 package main;
 
 import GUI.UserInterface;
-import images.ComplexPixel;
 import images.ImageManager;
-import mandelbrot.ComplexNumber;
 import mandelbrot.Parallel;
 import mandelbrot.Serial;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
-import java.awt.image.Raster;
-import java.util.Vector;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 
 public class Main {
 
-	private static final int imageWidth = 2000;
-	private static final int imageHeight = 2000;
+	private static final int imageWidth = 1000;
+	private static final int imageHeight = 1000;
+
+	private static ImageManager imageManager = new ImageManager(imageWidth,imageHeight);
 
 	static UserInterface GUI;
 
 	public static void main(String[] args) {
 
-		ImageManager imageManager = new ImageManager(imageWidth,imageHeight);
 
 		long t = System.currentTimeMillis();
 
@@ -41,23 +33,28 @@ public class Main {
 
 		Parallel t1, t2, t3, t4;
 
-		t1 = new Parallel(imageWidth,imageHeight,0,0,imageWidth/2, imageHeight /2);
-		t2 = new Parallel(imageWidth,imageHeight,imageWidth/2,0,imageWidth, imageHeight /2);
-		t3 = new Parallel(imageWidth,imageHeight,0,imageHeight /2 ,imageWidth/2, imageHeight);
-		t4 = new Parallel(imageWidth,imageHeight,imageWidth/2,imageHeight /2,imageWidth, imageHeight);
+		int imageSlice = imageHeight/10; //Runtime.getRuntime().availableProcessors();
+		System.out.println(imageSlice);
+
+
+
+		t1 = new Parallel("daemon", imageManager, 0, 0, imageSlice, imageHeight);
+		//t2 = new Parallel("daemon", imageManager, imageSlice, 0, imageSlice * 2, imageHeight);
+		//t3 = new Parallel("daemon", imageManager, imageSlice * 2, 0, imageSlice * 3, imageHeight);
+		//t4 = new Parallel("daemon", imageManager, imageSlice * 3, 0, imageSlice * 4, imageHeight);
 
 		t = System.currentTimeMillis();
 
 		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
+		//t2.start();
+		//t3.start();
+		//t4.start();
 
 		try {
 			t1.join();
-			t2.join();
-			t3.join();
-			t4.join();
+		//	t2.join();
+		//	t3.join();
+		//	t4.join();
 		}
 		catch (InterruptedException e) {
 			e.printStackTrace();
@@ -70,10 +67,9 @@ public class Main {
 		Graphics graphics = finalImage.getGraphics();
 
 		graphics.drawImage(t1.slice,0,0,null);
-		graphics.drawImage(t2.slice,0,0,null);
-		graphics.drawImage(t3.slice,0,0,null);
-		graphics.drawImage(t4.slice,0,0,null);
-
+		//graphics.drawImage(t2.slice,0,0,null);
+		//graphics.drawImage(t3.slice,0,0,null);
+		//graphics.drawImage(t4.slice,0,0,null);
 
 		imageManager.writeImage(finalImage,1);
 
--- src/mandelbrot/Parallel.java
+++ src/mandelbrot/Parallel.java
@@ -1,36 +1,157 @@
 package mandelbrot;
 
-import images.ComplexPixel;
 import images.ImageManager;
-import main.Main;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Robert on 2/18/2017.
+ *
  */
 public class Parallel extends Thread {
 
 	public BufferedImage slice;
 
-	private int width;
-	private int height;
-	private int x;
-	private int y;
+	private ImageManager imageManager;
+	private int startingX;
+	private int startingY;
+	private int endingX;
+	private int endingY;
 
-	public Parallel(int imageWidth, int imageHeight,int xStart, int yStart ,int xBound, int yBound) {
-		slice = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB_PRE);
-		width = xBound;
-		height = yBound;
-		x = xStart;
-		y = yStart;
+	public volatile int top_starting_X_T1 = 0;
+	public volatile int top_starting_Y_T1 = 0;
+
+	public volatile int bottom_starting_X_T2 = 0;
+	public volatile int bottom_starting_Y_T2 = 0;
+
+	public Parallel(String threadName, ImageManager imageManager) {
+		super(threadName);
+
+		//By getting a reference to the ImageManager we can get the size of the main image
+		this.imageManager = imageManager;
+
+		//Each thread needs a blank buffered image.
+		slice = new BufferedImage(imageManager.getImageWidth(), imageManager.getImageHeight(), BufferedImage.TYPE_INT_ARGB);
+
+	}
+
+	public Parallel(String threadName, ImageManager imageManager, int startingX, int startingY, int endingX, int endingY) {
+
+		super(threadName);
+
+		//By getting a reference to the ImageManager we can get the size of the main image
+		this.imageManager = imageManager;
+
+		//We need the bounds of the image
+		this.startingX  = top_starting_X_T1  = startingX;
+		this.startingY  = top_starting_Y_T1  = startingY;
+		this.endingX    = bottom_starting_X_T2  = endingX;
+		this.endingY    = bottom_starting_Y_T2  = endingY - 1;
+
+//		System.out.println(this.getId() + "top_starting_X_T1: " + top_starting_X_T1);
+//		System.out.println(this.getId() + "top_starting_Y_T1: " + top_starting_Y_T1);
+//		System.out.println(this.getId() + "bottom_starting_X_T2: " + bottom_starting_X_T2);
+//		System.out.println(this.getId() + "bottom_starting_Y_T2: " + bottom_starting_Y_T2);
+
+		//Each thread needs a blank buffered image.
+		slice = new BufferedImage(imageManager.getImageWidth(), imageManager.getImageHeight(), BufferedImage.TYPE_INT_ARGB);
+
 	}
 
 	@Override
 	public void run() {
 		super.run();
 
+		Parallel t1, t2;
+		t1 = null;
+		t2 = null;
+
+		switch (this.getName()){
+			case "daemon":
+
+				t1 = new Parallel("top", imageManager);
+				t2 = new Parallel("bottom", imageManager);
+
+				t1.start();
+				t2.start();
+
+				try{
+				 t1.join();
+				 t2.join();
+
+				}
+				catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				Graphics graphics = slice.getGraphics();
+				graphics.drawImage(t1.slice,0,0,null);
+				graphics.drawImage(t2.slice,0,0,null);
+
+				break;
+
+			case "top":
+
+				ComplexNumber top_c = new ComplexNumber();
+
+//				System.out.println(this.getId() + "top_starting_X_T1: " + top_starting_X_T1);
+//				System.out.println(this.getId() + "top_starting_Y_T1: " + top_starting_Y_T1);
+//				System.out.println(this.getId() + "bottom_starting_X_T2: " + bottom_starting_X_T2);
+//				System.out.println(this.getId() + "bottom_starting_Y_T2: " + bottom_starting_Y_T2);
+
+				//TODO: Remove hardcoded values
+//				for (; top_starting_Y_T1 < bottom_starting_Y_T2; top_starting_Y_T1++) {
+//					top_c.setImaginary(-1 * (top_starting_Y_T1 - (1000 / 2f)) * (1f / (1000 / 4f)));
+//					for (; (top_starting_X_T1 < bottom_starting_X_T2) && (bottom_starting_Y_T2 != top_starting_Y_T1); top_starting_X_T1++){
+//						top_c.setReal((top_starting_X_T1 - (1000 / 2f)) * (1f / (1000 / 4f)));
+//						testBehavior(top_c, top_starting_X_T1, top_starting_Y_T1);
+//					}
+//
+//				}
+				for (; t1.top_starting_Y_T1 < t2.bottom_starting_Y_T2; t1.top_starting_Y_T1++) {
+					top_c.setImaginary(-1 * (t1.top_starting_Y_T1 - (1000 / 2f)) * (1f / (1000 / 4f)));
+					for (int i = t1.top_starting_X_T1; i < t2.bottom_starting_X_T2; i++){
+						top_c.setReal((i - (1000 / 2f)) * (1f / (1000 / 4f)));
+						testBehavior(top_c, i, t1.top_starting_Y_T1);
+					}
+
+				}
+				break;
+
+			case "bottom":
+
+				ComplexNumber bottom_c = new ComplexNumber();
+
+//				System.out.println(this.getId() + "top_starting_X_T1: " + top_starting_X_T1);
+//				System.out.println(this.getId() + "top_starting_Y_T1: " + top_starting_Y_T1);
+//				System.out.println(this.getId() + "bottom_starting_X_T2: " + bottom_starting_X_T2);
+//				System.out.println(this.getId() + "bottom_starting_Y_T2: " + bottom_starting_Y_T2);
+
+//				for (; bottom_starting_Y_T2 > top_starting_Y_T1; bottom_starting_Y_T2--) {
+//					bottom_c.setImaginary(-1 * (bottom_starting_Y_T2 - (1000 / 2f)) * (1f / (1000 / 4f)));
+//					for (; (bottom_starting_X_T2 > top_starting_X_T1) && (bottom_starting_Y_T2 != top_starting_Y_T1); bottom_starting_X_T2--){
+//						bottom_c.setReal((bottom_starting_X_T2 - (1000 / 2f)) * (1f / (1000 / 4f)));
+//						System.out.println("Bottom X: "  + bottom_starting_X_T2 + "Bottom Y: " + bottom_starting_Y_T2);
+//						testBehavior(bottom_c, bottom_starting_X_T2, bottom_starting_Y_T2);
+//					}
+//					System.out.println("Bottom X: "  + bottom_starting_X_T2 + "Top X: " + top_starting_X_T1);
+//				}
+				for (; t2.bottom_starting_Y_T2 > t1.top_starting_Y_T1; t2.bottom_starting_Y_T2--) {
+					bottom_c.setImaginary(-1 * (t2.bottom_starting_Y_T2 - (1000 / 2f)) * (1f / (1000 / 4f)));
+					for (int i = t1.top_starting_X_T1; i < t2.bottom_starting_X_T2; i++){
+						bottom_c.setReal((i - (1000 / 2f)) * (1f / (1000 / 4f)));
+						testBehavior(bottom_c, i, t2.bottom_starting_Y_T2);
+					}
+
+				}
+
+				break;
+
+			default:
+				break;
+		}
 
 	}
 
@@ -43,7 +164,13 @@ public class Parallel extends Thread {
 		for (int k = 1; k < 1002; k++) {
 
 			if (m.magnitude() > 2) {
-				slice.setRGB(Math.round(widthX), Math.round(heightY), (0xFF << 24)| (0x007F7F7F / (k + 1)));
+				try {
+					slice.setRGB(Math.round(widthX), Math.round(heightY), (0xFF << 24)| (0x007F7F7F / (k + 1)));
+				}
+				catch (Exception e) {
+					System.err.println("The width is: " + widthX);
+					System.err.println("The height is: " + heightY);
+				}
 				break;
 			}
 
@@ -51,7 +178,13 @@ public class Parallel extends Thread {
 			z = m;
 
 			if (k == 1001) {
-				slice.setRGB(Math.round(widthX), Math.round(heightY), (0xFF << 24));
+				try{
+					slice.setRGB(Math.round(widthX), Math.round(heightY), (0xFF << 24));
+				}
+				catch (Exception e) {
+					System.err.println("The width is: " + widthX);
+					System.err.println("The height is: " + heightY);
+				}
 			}
 
 		}
